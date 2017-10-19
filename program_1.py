9771d6897b661dfd9d412954b8e4dcf8aea67a2e
jonleecutler <31291496+jonleecutler@users.noreply.github.com>
2017-10-18 22:24:45 -0400

    Updated password_database to parse passwords based on model.

--- program_1.py
+++ program_1.py
@@ -7,9 +7,13 @@ import password_model
 # TODO: Parse input parameters
 print(sys.argv[1:])
 
-# TODO: This is an example of loading the dataset and printing the count of password '123456'
-PASSWORDS = password_database.load_rockyou_dataset()
-print(PASSWORDS['123456'])
+# TODO: This is an example of loading the dataset and printing examples
+passwords = password_database.load_rockyou_dataset()
+print(passwords[password_model.PasswordModel.UNKNOWN][:10])
+print(passwords[password_model.PasswordModel.DIGITS_ONLY][:10])
+print(passwords[password_model.PasswordModel.LETTERS_ONLY][:10])
+print(passwords[password_model.PasswordModel.MIXED_LETTERS_DIGITS][:10])
+print(passwords[password_model.PasswordModel.WITH_SPECIAL_CHARS][:10])
 
 # This is an example testing the password_model find method
 # Outputs PasswordModel.DIGIT_ONLY
